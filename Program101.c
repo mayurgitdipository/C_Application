5c63fd9bea86309b1e10debea3790f9a382cf72f
Mayur Balasaheb Gapak <121565856+mayurgitdipository@users.noreply.github.com>
2023-02-28 10:27:38 +0530

    Add files via upload

---
+++ Program101.c
@@ -0,0 +1,37 @@
+// Row 	4
+// Column 	4
+/*
+	1	2	3	4
+	1	2	3	4
+	1	2	3	4
+	1	2	3	4
+
+*/
+	
+
+
+#include<stdio.h>
+
+
+
+void Display(int iRow ,int iCol)
+{
+
+
+}
+
+int main()
+{
+	int iValue1 = 0, iValue2 = 0;
+
+	printf("Enter the number of Row  \n");
+	scanf("%d",&iValue1);
+
+	printf("Enter the number of Column  \n");
+	scanf("%d",&iValue2);
+
+
+	Display(iValue1, iValue2);
+
+	return 0;
+}
